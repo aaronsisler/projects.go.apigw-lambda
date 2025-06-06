aec2ee9e88f7ba763394722deaaf82d754533b7d
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-06-06 09:23:16 -0400

    Changing how the response is coming back to be JSON

--- handlers/hello_get_handler.go
+++ handlers/hello_get_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/aaronsisler/projects.go.apigw-lambda/shared"
 	"github.com/aws/aws-lambda-go/events"
@@ -12,11 +13,19 @@ func HelloGetHandler(ctx context.Context, req events.APIGatewayProxyRequest) (ev
 
 	message := shared.FormatMessage(name)
 
+	responseBody, err := json.Marshal(map[string]string{
+		"message": "Handler: Hello: GET: " + message,
+	})
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal Server Error"}, nil
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
-		Body: "Handler: Hello: GET: " + message,
+		Body: string(responseBody),
 	}, nil
 }
